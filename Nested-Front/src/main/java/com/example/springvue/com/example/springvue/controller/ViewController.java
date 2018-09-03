acc729e4e2224782ac629c73c2821076be1daebc
조세진 <sejinjja@bpnr.co.kr>
2018-09-03 11:14:26 +0900

    프로젝트 루트 변경

---
+++ Nested-Front/src/main/java/com/example/springvue/com/example/springvue/controller/ViewController.java
@@ -0,0 +1,21 @@
+package com.example.springvue.com.example.springvue.controller;
+
+import com.example.springvue.com.example.springvue.dao.MockStoreDao;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+public class ViewController {
+    @RequestMapping(path = "{store}/index.html", method = RequestMethod.GET)
+    public String test(@PathVariable("store") String store, ModelMap model) {
+        MockStoreDao mockStoreDao = MockStoreDao.getInstance();
+
+        model.addAttribute("storeInfo", mockStoreDao.getInfo(store));
+
+        return "index.html";
+    }
+}
