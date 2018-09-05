7472c815fe8f9f91429aaa97c9970ff09a8b6a92
조세진 <sejinjja@bpnr.co.kr>
2018-09-05 09:06:13 +0900

    template 캐시 설정 추가.

--- ExternalResources/BackEnd/src/main/java/com/example/backend/controller/IndexController.java
+++ ExternalResources/BackEnd/src/main/java/com/example/backend/controller/IndexController.java
@@ -1,18 +1,15 @@
 package com.example.backend.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.support.ResourcePatternResolver;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
 public class IndexController {
-    @Autowired
-    ResourcePatternResolver resourcePatternResolver;
 
     @RequestMapping("/")
     public String index() {
+
         return "index.html";
     }
 }
